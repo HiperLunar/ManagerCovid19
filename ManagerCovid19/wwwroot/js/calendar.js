a77d093307fb383df9034895d95173c85006a1e9
felipe bernardino <felipe.kelemen02@gmail.com>
2021-11-08 13:08:40 +0200

    * Fix markation bug

--- ManagerCovid19/wwwroot/js/calendar.js
+++ ManagerCovid19/wwwroot/js/calendar.js
@@ -1,85 +1,102 @@
-﻿const date = new Date();
-var selectedDate;
+﻿
+var calendar = {
+    date: new Date(),
+    selectedDate: null,
 
-const renderCalendar = () => {
-    date.setDate(1);
+    data: [],
 
-    const firstDayIndex = date.getDay();
-    const firstDate = new Date(date.getFullYear(), date.getMonth(), 1 - firstDayIndex);
+    loadData: function () {},
+    mark: function (date) { return false; },
 
-    const months = [
-        "Janeiro",
-        "Fevereiro",
-        "Março",
-        "Abril",
-        "Maio",
-        "Junho",
-        "Julho",
-        "Agosto",
-        "Setembro",
-        "Outubro",
-        "Novembro",
-        "Dezembro"
-    ];
+    renderCalendar: function () {
 
-    document.querySelector(".date h1").innerHTML = months[date.getMonth()];
-    document.querySelector(".date p").innerHTML = new Date().toDateString();
+        this.loadData(this.date.getMonth());
 
-    $('.days').empty();
+        this.date.setDate(1);
 
-    while (firstDate.getMonth() <= date.getMonth() || firstDate.getDay() != 0) {
-        var day = jQuery('<div>');
-        day.html(firstDate.getDate());
-        if (mark(firstDate)) day.addClass('marked');
-        if (firstDate.getDate() == new Date().getDate()
-            && firstDate.getMonth() == new Date().getMonth()
-            && firstDate.getFullYear() == new Date().getFullYear())
-            day.addClass('today');
-        else if (firstDate.getMonth() < date.getMonth())
-            day.addClass('prev-date');
-        else if (firstDate.getMonth() > date.getMonth())
-            day.addClass('next-date');
-        if (selectedDate)
-            if (firstDate.getFullYear() == selectedDate.getFullYear()
-                && firstDate.getMonth() == selectedDate.getMonth()
-                && firstDate.getDate() == selectedDate.getDate())
-                day.addClass('selected');
+        const firstDayIndex = this.date.getDay();
+        const firstDate = new Date(this.date.getFullYear(), this.date.getMonth(), 1 - firstDayIndex);
 
-        firstDate.setDate(firstDate.getDate() + 1);
-        day.appendTo('.days');
+        const months = [
+            "Janeiro",
+            "Fevereiro",
+            "Março",
+            "Abril",
+            "Maio",
+            "Junho",
+            "Julho",
+            "Agosto",
+            "Setembro",
+            "Outubro",
+            "Novembro",
+            "Dezembro"
+        ];
+
+        document.querySelector(".date h1").innerHTML = months[this.date.getMonth()];
+        document.querySelector(".date p").innerHTML = new Date().toDateString();
+
+        $('.days').empty();
+
+        while (firstDate.getMonth() <= this.date.getMonth() || firstDate.getDay() != 0) {
+            var day = jQuery('<div>');
+            day.html(firstDate.getDate());
+            if (this.mark(firstDate)) day.addClass('marked');
+            if (firstDate.getDate() == new Date().getDate()
+                && firstDate.getMonth() == new Date().getMonth()
+                && firstDate.getFullYear() == new Date().getFullYear())
+                day.addClass('today');
+            else if (firstDate.getMonth() < this.date.getMonth())
+                day.addClass('prev-date');
+            else if (firstDate.getMonth() > this.date.getMonth())
+                day.addClass('next-date');
+            if (this.selectedDate)
+                if (firstDate.getFullYear() == this.selectedDate.getFullYear()
+                    && firstDate.getMonth() == this.selectedDate.getMonth()
+                    && firstDate.getDate() == this.selectedDate.getDate())
+                    day.addClass('selected');
+
+            day.attr('year', firstDate.getFullYear())
+            day.attr('month', firstDate.getMonth())
+            day.attr('date', firstDate.getDate())
+
+            firstDate.setDate(firstDate.getDate() + 1);
+            day.appendTo('.days');
+        }
     }
 }
 
 $('.prev').click(() => {
-    date.setMonth(date.getMonth() - 1)
-    renderCalendar();
+    calendar.date.setMonth(calendar.date.getMonth() - 1)
+    calendar.renderCalendar();
 });
 
 $('.next').click(() => {
-    date.setMonth(date.getMonth() + 1)
-    renderCalendar();
+    calendar.date.setMonth(calendar.date.getMonth() + 1)
+    calendar.renderCalendar();
 });
 
 $('.days').click('div', element => {
     if (element.target.classList.contains('prev-date'))
-        selectedDate = new Date(
-            date.getFullYear(),
-            date.getMonth() - 1,
+        calendar.selectedDate = new Date(
+            calendar.date.getFullYear(),
+            calendar.date.getMonth() - 1,
             element.target.innerText
         );
     else if (element.target.classList.contains('next-date'))
-        selectedDate = new Date(
-            date.getFullYear(),
-            date.getMonth() + 1,
+        calendar.selectedDate = new Date(
+            calendar.date.getFullYear(),
+            calendar.date.getMonth() + 1,
             element.target.innerText
         );
     else
-        selectedDate = new Date(
-            date.getFullYear(),
-            date.getMonth(),
+        calendar.selectedDate = new Date(
+            calendar.date.getFullYear(),
+            calendar.date.getMonth(),
             element.target.innerText
         );
-    renderCalendar();
+
+    console.log(calendar.selectedDate);
+    calendar.renderCalendar();
 });
 
-renderCalendar();
+calendar.renderCalendar();
